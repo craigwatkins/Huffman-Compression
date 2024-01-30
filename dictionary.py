a2033fed15f4e50b9a2cb9ecae1942e7547458fc
craig <craigwatkins@rocketmail.com>
2024-01-29 21:56:35 -0800

    adding new classes

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+"""
+Dictionary class
+properties: huffman_tree, huffman_dictionary, canonical huffman dictionary
+methods: build_huffman_tree, build_huffman_dictionary, build_canonical_huffman_dictionary, get_canonical_huffman_dictionary
+"""
+
+
+class Dictionary:
+    def __init__(self):
+        self.huffman_tree = None
+        self.huffman_dictionary = None
+        self.canonical_huffman_dictionary = None
+
+    def build_huffman_tree(self, data):
+        pass
+
+
