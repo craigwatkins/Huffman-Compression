1fe89fb7bad615b47d4b0f7d75476f459612cb04
craig <craigwatkins@rocketmail.com>
2024-01-31 20:01:59 -0800

    Updated init file for easier use.

--- canonical_huffman/__init__.py
+++ canonical_huffman/__init__.py
@@ -1,3 +1,3 @@
 from .huffman_compression import HuffmanCoding
-from .dictionary import HuffmanDictionary
+from .huffman_compression import HuffmanDictionary
 from .padded_binary import write_padded_bytes, read_padded_bytes
